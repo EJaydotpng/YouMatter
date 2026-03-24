eef64c0b63c4d9d29e98222a6b8dfd25a23524f7
Elija <elija.sabulao@valens-research.com>
2026-03-24 17:06:39 +0800

    changes

--- app/settings.py
+++ app/settings.py
@@ -18,7 +18,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Load .sample.env file
-load_dotenv(BASE_DIR / '.sample.env')
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
