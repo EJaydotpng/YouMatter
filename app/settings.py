2df1d6f2f807b35cd87abc40a619c52282834793
Elija <elija.sabulao@valens-research.com>
2026-03-24 17:05:50 +0800

    changes

--- app/settings.py
+++ app/settings.py
@@ -17,8 +17,8 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Load .sample.env file
-load_dotenv(BASE_DIR / '.sample.env')
+# Load .env file
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
